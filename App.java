811493deb8f71333e3ac8aee89d894c0e2aeb486
Nikith Ganga <91150345+NikithGanga@users.noreply.github.com>
2024-04-08 11:42:10 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int result = sum(a, b);
+        System.out.println("Sum of " + a + " and " + b + " is: " + result);
+    }
+}
+
